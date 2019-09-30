a31a8c735c44a3d4265f7043a27171713416e1d0
justinpotts <justin@justinpotts.co>
2019-09-30 17:15:48 -0500

    Add component files

--- install.sh
+++ install.sh
@@ -15,3 +15,10 @@ source blueprint-lib/init.sh
 # BASE_PATH is the full path to the project root
 # APP_NAME is the name of the Django app that will be modified
 ##
+
+# Copy dashboard static files
+mkdir -p $BASE_PATH/static/vendor/dashboard/
+cp -R blk/ $BASE_PATH/static/vendor/dashboard/
+
+# Copy base, charts, notifications, profile, and table templates
+cp -R templates/ $BASE_PATH/home/templates
